adf1d36dd2c5fb556bdfe17f43d46537b76f2e0f
Vitor Pantoja <jose.travassos@dellead.com>
2023-02-20 18:38:44 -0300

    feat
    Base construction of the project and division of its layers.

---
+++ Manager.Domain/Entities/User.cs
@@ -0,0 +1,62 @@
+﻿using Manager.Domain.Validators;
+
+namespace Manager.Domain.Entities;
+
+public class User : Base
+{
+    
+    public string Name { get; private set; }
+
+    public string Email { get; private set; }
+
+    public string Password { get; private set; }
+    
+    public User(string name, string email, string password)
+    {
+        Name = name;
+        Email = email;
+        Password = password;
+        _errors = new List<string>();
+    }
+    
+    //EntityFramework
+    protected User() {}
+
+    public void ChangeName(string name)
+    {
+        Name = name;
+        Validate();
+    }
+
+    public void ChangePassword(string password)
+    {
+        Password = password;
+        Validate();
+    }
+
+    public void ChangeEmail(string email)
+    {
+        Email = email;
+        Validate();
+    }
+
+    public override bool Validate()
+    {
+        var validator = new UserValidator();
+        var validation = validator.Validate(this);
+        
+        if (!validation.IsValid)
+        {
+            validation.Errors.ForEach(error =>
+            {
+                _errors.Add(error.ErrorMessage);
+                throw new Exception("Alguns campos estão inválidos, por favor corrija-os!" + _errors[0]);
+            });
+            // foreach (var error in validation.Errors)
+            // {
+            //     _errors.Add(error.ErrorMessage);
+            // }
+        }
+        return true;
+    }
+}
---
+++ Manager.Domain/Validators/UserValidator.cs
@@ -0,0 +1,63 @@
+﻿using FluentValidation;
+using Manager.Domain.Entities;
+
+namespace Manager.Domain.Validators;
+
+public class UserValidator : AbstractValidator<User>
+{
+    public UserValidator()
+    {
+        RuleFor(user => user)
+            .NotEmpty()
+            .WithMessage("A entidade não pode ser vazia")
+            
+            .NotNull()
+            .WithMessage("A entidade não pode ser nula!");
+
+        RuleFor(user => user.Name)
+            .NotNull()
+            .WithMessage("O nome não pode ser nulo!")
+
+            .NotEmpty()
+            .WithMessage("O nome não pode ser vazio!")
+            
+            .MinimumLength(3)
+            .WithMessage("O nome deve ter no mínimo 3 caracteres")
+            
+            .MaximumLength(80)
+            .WithMessage("O nome deve ter no máximo 80 caracteres");
+        
+        RuleFor(user => user.Password)
+            .NotNull()
+            .WithMessage("A senha não pode ser nulo!")
+
+            .NotEmpty()
+            .WithMessage("A senha não pode ser vazio!")
+            
+            .MinimumLength(6)
+            .WithMessage("A senha deve ter no mínimo 6 caracteres")
+            
+            .MaximumLength(30)
+            .WithMessage("A senha deve ter no máximo 30 caracteres")
+            
+            .Matches(@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$")
+            .WithMessage("A senha informado não é válido!");
+        
+        RuleFor(user => user.Email)
+            .NotNull()
+            .WithMessage("O email não pode ser nulo!")
+
+            .NotEmpty()
+            .WithMessage("O email não pode ser vazio!")
+            
+            .MinimumLength(10)
+            .WithMessage("O email deve ter no mínimo 10 caracteres")
+            
+            .MaximumLength(180)
+            .WithMessage("O email deve ter no máximo 180 caracteres")
+            
+            .Matches(@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$")
+            .WithMessage("O email informado não é válido!");
+    }    
+}
+//to-do: incrementar validações 
